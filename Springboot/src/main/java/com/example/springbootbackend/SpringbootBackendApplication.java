bba53432458bb0d0cf897cfad89fafd73f1184f9
Alex <kohan_alex@yahoo.com>
2024-02-03 04:18:44 +0200

    commit

--- Springboot/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
+++ Springboot/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:first.properties")
+//@PropertySource("classpath:first.properties")
 public class SpringbootBackendApplication {
 
     public static void main(String[] args) {
