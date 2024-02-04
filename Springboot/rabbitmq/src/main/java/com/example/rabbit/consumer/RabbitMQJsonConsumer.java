89f59bddc0b503f6a372c7381ea1e707b3ac26fe
Alex <kohan_alex@yahoo.com>
2024-02-05 00:56:25 +0200

    .....

--- Springboot/rabbitmq/src/main/java/com/example/rabbit/consumer/RabbitMQJsonConsumer.java
+++ Springboot/rabbitmq/src/main/java/com/example/rabbit/consumer/RabbitMQJsonConsumer.java
@@ -1,9 +1,12 @@
 package com.example.rabbit.consumer;
 
 import com.example.rabbit.dto.Contact;
+import com.example.rabbit.model.Employee;
+import com.example.rabbit.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +14,13 @@ public class RabbitMQJsonConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
     public void consumeJsonMessage(Contact contact){
         LOGGER.info("convert message!!!!!!!");
+        employeeRepository.save(new Employee("a","a","a"));
         LOGGER.info(String.format("Received JSON message -> %s", contact.toString()));
     }
 }
