d51826972f5542896ba3252e0e72dab85558680d
Alex <kohan_alex@yahoo.com>
2024-02-06 11:59:06 +0200

    small changes

--- Springboot/backend/src/main/java/com/example/springbootbackend/controller/OrderController.java
+++ Springboot/backend/src/main/java/com/example/springbootbackend/controller/OrderController.java
@@ -1,12 +1,11 @@
 package com.example.springbootbackend.controller;
 
+import com.example.springbootbackend.exception.ResourceNotFoundException;
 import com.example.springbootbackend.model.Order;
 import com.example.springbootbackend.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,16 @@ public class OrderController {
     @GetMapping("/orders")
     public List<Order> getAllOrders(){
         return orderRepository.findAll();
+    }
+
+    // build delete order REST API
+    @DeleteMapping("/orders/{id}")
+    public ResponseEntity<Order> deleteOrder(@PathVariable long id){
+        Order order=orderRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Order not exist with id" + id));
+
+        orderRepository.delete(order);
+
+        return ResponseEntity.ok(order);
     }
 }
