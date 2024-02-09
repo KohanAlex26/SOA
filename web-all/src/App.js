d778b876f8eaff976720b51510ff61a257f1ab9c
Alex <kohan_alex@yahoo.com>
2024-02-09 13:25:31 +0200

    small micro frontend with iframes

--- web-all/src/App.js
+++ web-all/src/App.js
@@ -5,16 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, React!</h1>
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        <iframe title="frontend-1" src="http://localhost:3000" width="700" height="600" />
+      </div>
+      <div>
+        <iframe title="frontend-2" src="http://localhost:3001" width="700" height="200" />
+      </div>
+      <div>
+        <iframe title="frontend-3" src="http://localhost:3002" width="700" height="300" />
+      </div>
       </header>
     </div>
   );
